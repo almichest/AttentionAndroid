e4e3783ed1b1284e1677ca4021884d2ba1f90a2d
Hiraku Ohno <almichest.free@gmail.com>
2016-09-12 01:26:53 +0900

    Enable using original fragment.

--- app/src/main/kotlin/hira/attentionandroid/fragment/MapFragment.kt
+++ app/src/main/kotlin/hira/attentionandroid/fragment/MapFragment.kt
@@ -1,16 +1,13 @@
 package hira.attentionandroid.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import hira.attentionandroid.R
 
 /**
  * Created by hira on 2016/09/11.
@@ -18,14 +15,11 @@ import hira.attentionandroid.R
 
 class MapFragment: SupportMapFragment(), OnMapReadyCallback {
 
-    private var map: GoogleMap? = nil
+    private var map: GoogleMap? = null
 
-    override fun onCreateView(layoutInflater: LayoutInflater?, viewGroup: ViewGroup?, bundle: Bundle?): View {
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        val mapFragment = context.supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
-
-        return layoutInflater.inflate(R.)
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getMapAsync(this)
     }
 
     override fun onMapReady(map: GoogleMap?) {
